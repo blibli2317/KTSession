446a7e5e9eb2d54d1720eea24921902758746d7a
Vishnu.s <Vishnu.s@gdn-commerce.com>
2023-01-10 13:39:56 +0530

    my first commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * A simple unit test
+ */
+public class MainTest
+{
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
